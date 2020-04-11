02e2c2dfa6772c12d0869bbdb817714c2b9b4ff3
Mihai Smarandache <mihai.sanfran@gmail.com>
2020-04-10 17:02:47 -0700

    Connect to MongoDB

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const morgan = require('morgan')
+const dotenv = require('dotenv')
+dotenv.config()
+
+// db
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('DB Connected')
+    })
+
+mongoose.connection.on('error', err => {
+    console.log('DB connection error: ', err.message)
+})
 
 // Bring in routes
 const postRoutes = require('./routes/post')
