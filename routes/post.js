49d96f644d73402ba216980d5e325f38a2a623e1
Mihai Smarandache <mihai.sanfran@gmail.com>
2020-04-11 16:54:15 -0700

    Code refactor

--- routes/post.js
+++ routes/post.js
@@ -1,14 +1,12 @@
 const express = require('express')
-const postController = require('../controllers/post')
+const { getPosts, createPost } = require('../controllers/post')
 const validator = require('../validator')
 const router = express.Router()
 
-router.get('/', postController.getPosts)
+router.get('/', getPosts)
 
-//console.log(process)
 // Only if it passes the middleware (validator) - it will create the post
-router.post('/post', validator.createPostValidator, postController.createPost)
-//router.post('/post', postController.createPost)
+router.post('/post', validator.createPostValidator, createPost)
 
 
 module.exports = router
